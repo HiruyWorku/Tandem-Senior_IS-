af3a9029d611d0ffcad8b73c8c98445b5a32e5cd
Hiruy Worku <hworku27@wooster.edu>
2025-09-09 22:46:16 -0400

    build initial skeleton, tested basic functionality

--- video-call/src/App.tsx
+++ video-call/src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+  type User,
+} from '@stream-io/video-react-sdk';
 
-function App() {
-  const [count, setCount] = useState(0)
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import './index.css';
 
+const apiKey = 'mmhfdzb5evj2';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1NwaWNlX0RvbHBoaW4iLCJ1c2VyX2lkIjoiU3BpY2VfRG9scGhpbiIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzU3NDY2NjAyLCJleHAiOjE3NTgwNzE0MDJ9.y2aOp5N0g7tYDN_UyRbvJEowjq6CUd2MpiuZY4ZrMmw';
+const userId = 'Spice_Dolphin';
+const callId = 'bHqanl5wXF14nipGI1GMJ';
+
+const user: User = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('default', callId);
+call.join({ create: true });
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
 }
 
-export default App
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition='bottom' />
+      <CallControls />
+    </StreamTheme>
+  );
+};
